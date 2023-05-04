306565dc91ddbf3345b6fb72d6845d480fb48a1b
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2023-05-04 14:17:37 -0600

    Include example dataset

---
+++ src/borg_parser/datasets.py
@@ -0,0 +1,16 @@
+"""Default datasets included with Paxplot"""
+
+import pkg_resources
+
+
+def water_energy():
+    """
+    Water/Energy trade-off dataset
+
+    Returns
+    -------
+    stream : _io.BufferedReader
+        Stream of trade-off dataset
+    """
+    stream = pkg_resources.resource_stream(__name__, 'example_runtime/water_energy.txt')
+    return stream
